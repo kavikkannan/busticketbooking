1d132b7da251b7342f0bcc2d42a00657c23ed19f
subi <rsubikksha@gmail.com>
2023-12-01 13:24:25 +0530

    1-12-23:1:24

--- components/Body_LPage.jsx
+++ components/Body_LPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BusTicketBookingLanding = () => {
   return (
-    <div className="py-12 bg-[url(&apos;/images/bg1.jpg&apos;)] bg-cover">
+    <div className="py-12 bg-[url('/images/bg1.jpg')] bg-cover">
       <div className="container mx-auto px-4">
         <h1 className="text-3xl text-green-200 font-bold mb-4">Welcome to Our Bus Ticket Booking System</h1>
         <p className="text-lg text-white mb-8">
